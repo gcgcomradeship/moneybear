f252796df6860c5037c9e0ef78fdb4c7408415a2
NS <kolud@bk.ru>
2019-09-29 19:26:03 +0300

    initial

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule Moneybear.MixProject do
     [
       app: :moneybear,
       version: "0.1.0",
-      elixir: "~> 1.5",
+      elixir: "~> 1.8.2",
       elixirc_paths: elixirc_paths(Mix.env()),
       compilers: [:phoenix, :gettext] ++ Mix.compilers(),
       start_permanent: Mix.env() == :prod,
@@ -42,7 +42,9 @@ defmodule Moneybear.MixProject do
       {:phoenix_live_reload, "~> 1.2", only: :dev},
       {:gettext, "~> 0.11"},
       {:jason, "~> 1.0"},
-      {:plug_cowboy, "~> 2.0"}
+      {:plug_cowboy, "~> 2.0"},
+      {:httpoison, "~> 1.5"},
+      {:gun, "~> 1.3"}
     ]
   end
 
