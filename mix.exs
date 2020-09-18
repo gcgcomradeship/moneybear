96aa2f51a83ab7033d4ae42805a77aa57dfa2652
NS <kolud@bk.ru>
2020-09-18 14:48:20 +0300

    add versions

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule Moneybear.MixProject do
     [
       app: :moneybear,
       version: "0.1.0",
-      elixir: "~> 1.9",
+      elixir: "~> 1.10",
       elixirc_paths: elixirc_paths(Mix.env()),
       compilers: [:phoenix, :gettext] ++ Mix.compilers(),
       start_permanent: Mix.env() == :prod,
